9d4ecd8f0fef8a165939f5e57854733a52e1f7e5
IlluminatorWatch <adam@pluxemburg.com>
2020-04-05 21:14:54 +0200

    now it is heroku time once again again

---
+++ config/config.js
@@ -0,0 +1,6 @@
+// Detta är ett objekt som vi kopierar in ifrån mongo compass-länken, glöm inte lösenordet!
+const config = {
+    // cloud för att det är det namnet som jag har i mongodb compass //
+    databaseURL:"mongodb+srv://Adam:King10Kong@cluster0-mqceb.mongodb.net/todo"
+}
+module.exports = config;
